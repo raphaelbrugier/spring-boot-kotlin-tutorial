6f9690e25d0b3b026d180f150372e4748e57fbf6
Raphael BRUGIER <raphael.brugier@gmail.com>
2018-05-13 18:00:00 -0400

    Add markdown support

--- src/main/kotlin/blog/HtmlController.kt
+++ src/main/kotlin/blog/HtmlController.kt
@@ -1,17 +1,53 @@
 package blog.web
 
+import blog.Article
+import blog.ArticleRepository
+import blog.MarkdownConverter
+import blog.format
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import blog.User
 
 @Controller
-class HtmlController {
+class HtmlController(private val repository: ArticleRepository,
+                     private val markdownConverter: MarkdownConverter) {
 
     @GetMapping("/")
     fun blog(model: Model): String {
         model["title"] = "Blog"
+        model["articles"] = repository.findAllByOrderByAddedAtDesc().map { it.render() }
         return "blog"
     }
+
+    @GetMapping("/article/{id}")
+    fun article(@PathVariable id: Long, model: Model): String {
+        val article = repository
+                .findById(id)
+                .orElseThrow { IllegalArgumentException("Wrong article id provided") }
+                .render()
+        model["title"] = article.title
+        model["article"] = article
+        return "article"
+    }
+
+    fun Article.render() = RenderedArticle(
+            title,
+            markdownConverter.invoke(headline),
+            markdownConverter.invoke(content),
+            author,
+            id,
+            addedAt.format()
+    )
+
+    data class RenderedArticle(
+            val title: String,
+            val headline: String,
+            val content: String,
+            val author: User,
+            val id: Long?,
+            val addedAt: String)
 
 }
--- src/test/kotlin/blog/IntegrationTests.kt
+++ src/test/kotlin/blog/IntegrationTests.kt
@@ -27,12 +27,15 @@ class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
         println(">> Assert blog page title, content and status code")
         val entity = restTemplate.getForEntity<String>("/")
         assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
-        assertThat(entity.body).contains("<h1>Blog</h1>")
+        assertThat(entity.body).contains("<h1>Blog</h1>", "Reactor")
     }
 
     @Test
     fun `Assert article page title, content and status code`() {
-        println(">> TODO")
+        println(">> Assert article page title, content and status code")
+        val entity = restTemplate.getForEntity<String>("/article/2")
+        assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(entity.body).contains("<a href=\"https://projectreactor.io/\">https://projectreactor.io/</a>")
 
     }
 
@@ -40,5 +43,4 @@ class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
     fun teardown() {
         println(">> Tear down")
     }
-
 }
